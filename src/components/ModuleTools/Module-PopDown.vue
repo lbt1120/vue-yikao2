f53633d06ae0cffbc640934ae880b414b8bab477
lbt1120 <410291696@qq.com>
2018-04-17 17:14:22 +0800

    后期删除，使用mint-ui替换

---
+++ src/components/ModuleTools/Module-PopDown.vue
@@ -0,0 +1,33 @@
+<template>
+<section class="popdownwapper" :class="{'popdownhidden':popdownbox==false}">
+  <div class="zz transition-5s"></div>
+  <div class="popdownwapper-content white_bg transition-5s">
+    <slot>
+    </slot>
+  </div>
+</section>
+</template>
+<script>
+export default {
+  name: 'popdown',
+  data() {
+    return {
+      popdownbox: false,
+    }
+  },
+  computed: {
+    // 实时监测vuex里显示状态的数据变化
+    popdownboxStatus: function() {
+      return this.$store.state.popdownshow;
+    }
+  },
+  watch: {
+    // 如果发生改变则赋值
+    popdownboxStatus: function(newVal, oldVal) {
+      if (newVal != oldVal) {
+        this.popdownbox = newVal;
+      }
+    }
+  },
+}
+</script>
