92296ca582a58fe0d697b1d19cffc15ef8a350dc
lbt1120 <410291696@qq.com>
2018-04-18 16:34:48 +0800

    数据缺失页面

---
+++ src/view/NotFound/View-NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="datanotfound wapper white_bg">
+    <div class="notfoundbox">
+      <div class="notfoundimg"></div>
+      <div class="notfoundtext font32">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
