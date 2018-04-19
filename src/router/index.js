239bd8b3f84cb2923883c4a24d7c1f04cf36695c
lbt1120 <410291696@qq.com>
2018-04-19 17:00:52 +0800

    添加内容页路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/view/home/router/index'
+import content from '@/view/content/router/index'
+
 // import institution from '@/view/institution/router/index'
 // import login from '@/view/login/router/index'
 // import user from '@/view/user/router/index'
-// import content from '@/view/content/router/index'
 // import order from '@/view/deal/router/index'
 // import school from '@/view/school/router/index'
 // import repository from '@/view/repository/router/index'
@@ -14,10 +15,10 @@ Vue.use(Router)
 export default new Router({
   routes: [
     ...home,
+    ...content,
     // ...institution,
     // ...login,
     // ...user,
-    // ...content,
     // ...order,
     // ...school,
     // ...repository
