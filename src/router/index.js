efcb9a2da3212728a9d346f4b003809b4e6efa13
lbt1120 <410291696@qq.com>
2018-04-23 18:21:17 +0800

    添加机构详情页路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/view/home/router/index'
 import content from '@/view/content/router/index'
-
-// import institution from '@/view/institution/router/index'
-// import login from '@/view/login/router/index'
+import institution from '@/view/Institution/router/index'
+import login from '@/view/Login/router/index'
 // import user from '@/view/user/router/index'
 // import order from '@/view/deal/router/index'
 // import school from '@/view/school/router/index'
@@ -16,8 +15,8 @@ export default new Router({
   routes: [
     ...home,
     ...content,
-    // ...institution,
-    // ...login,
+    ...institution,
+    ...login,
     // ...user,
     // ...order,
     // ...school,
