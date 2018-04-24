aa88aafde55767231e8d0da5faf66113461d36a3
lbt1120 <410291696@qq.com>
2018-04-24 18:08:40 +0800

    因后端传回数据不同，添加一个新的组件

---
+++ src/components/ModuleSwiper/Swiper-InsDetails.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="picbox">
+  <swiper :options="swiperOption" ref="mySwiper">
+    <swiper-slide v-for="(item,index) in datalist" :key="index">
+      <img :src="item" alt="">
+    </swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+<script>
+import {
+  swiper,
+  swiperSlide
+} from 'vue-awesome-swiper'
+
+export default {
+  name: 'swiper_home',
+  data() {
+    return {
+      bannerdata: "",
+      swiperOption: {
+        paginationClickable: true,
+        pagination: '.swiper-pagination',
+        grabCursor: true,
+        autoplay: 4000,
+        loop: true,
+        initialSlide: 1, //从0开始
+      },
+    }
+  },
+  //定义这个sweiper对象
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.swiper;
+    }
+  },
+  components: {
+    swiper,
+    swiperSlide
+  },
+  props: ['datalist'],
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.swiper-container {
+    height: 15.6rem;
+}
+
+.swiper-container img {
+    height: 100%;
+}
+</style>
