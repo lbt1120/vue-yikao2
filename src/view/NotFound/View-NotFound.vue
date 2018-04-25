3f773a10d02eeff6b3bbc6f2b66befc5934de949
lbt1120 <410291696@qq.com>
2018-04-25 17:56:43 +0800

    修改提示内容样式

--- src/view/NotFound/View-NotFound.vue
+++ src/view/NotFound/View-NotFound.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="datanotfound wapper white_bg">
-    <div class="notfoundbox">
-      <div class="notfoundimg"></div>
-      <div class="notfoundtext font32">
-        <slot></slot>
-      </div>
+<div class="datanotfound wapper white_bg">
+  <div class="notfoundbox">
+    <div class="notfoundimg"></div>
+    <div class="notfoundtext font32">
+      <slot></slot>
     </div>
   </div>
+</div>
 </template>
+<style scoped lang="less">
+.datanotfound {
+    padding-top: 10rem;
+}
+</style>
